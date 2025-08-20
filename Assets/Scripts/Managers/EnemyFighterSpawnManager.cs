4fac15ba0ba32a22f6c3ea995e2a5cf883579741
Yusuf Ayvaz <yusufayvaz202@gmail.com>
2025-08-20 11:07:47 +0300

    Add level data system and refactor enemy spawn logic
    Introduced LevelDataSO ScriptableObject to define level structure, waves, and units. Refactored EnemyFighterSpawnManager to use level data for spawning logic, supporting multiple waves and unit types. Moved fighter data assets into a Fighters subfolder and added initial level asset. Updated SampleScene to reference new level data and fighter prefabs.

--- Assets/Scripts/Managers/EnemyFighterSpawnManager.cs
+++ Assets/Scripts/Managers/EnemyFighterSpawnManager.cs
@@ -2,26 +2,32 @@
 using Abstracts;
 using Misc;
 using ObjectPooling;
+using ScriptableObjects;
 using UnityEngine;
 
 namespace Managers
 {
     public class EnemyFighterSpawnManager : MonoBehaviour
     {
+
         [Header("References")] 
         [SerializeField] private BaseFighter[] _baseFighterPrefab;
+        [SerializeField] private LevelDataSO _currentLevelData;
         [SerializeField] private Transform _target;
         [SerializeField] private Transform _spawnPosition;
         [SerializeField] private Transform _spawnParent;
-        [SerializeField] private bool _isSpawning = true;
         [SerializeField] private float _timeBetweenSpawns = 0.2f;
-        
-        [Header("Settings")]
+
+        [Header("Settings")] 
         private Coroutine _spawnCoroutine;
         private ObjectPool _fighterPool;
+        private int _currentWaveIndex;
+        private int _currentUnitIndex;
+        private int _currentSpawnCount;
+        private bool _isPaused;
 
         #region Unity Methods
-        
+
         private void OnEnable()
         {
             EventManager.OnGameStateChanged += OnGameStateChanged;
@@ -41,14 +47,67 @@ namespace Managers
 
         #endregion
 
+        
         private IEnumerator SpawnFighter()
         {
-            while (_isSpawning)
+            for (int w = _currentWaveIndex; w < _currentLevelData.Waves.Count; w++)
             {
-                yield return new WaitForSeconds(_timeBetweenSpawns);
-                BaseFighter baseFighter = _fighterPool.GetFighter(FighterType.CaveMan);
-                baseFighter.transform.position = _spawnPosition.position;
-                baseFighter.SetTargetDestination(_target);
+                var wave = _currentLevelData.Waves[w];
+                for (int u = _currentUnitIndex; u < wave.Units.Count; u++)
+                {
+                    var unit = wave.Units[u];
+                    for (int i = _currentSpawnCount; i < unit.UnitSpawnCount; i++)
+                    {
+                        BaseFighter baseFighter = _fighterPool.GetFighter(unit.FighterType);
+                        baseFighter.transform.position = _spawnPosition.position;
+
+                        _currentSpawnCount++;
+
+                        yield return WaitForSecondsRealtimeCustom(_currentLevelData.TimeBetweenEachFighter, () => _isPaused);
+                    }
+
+                    _currentUnitIndex = u + 1;
+                    _currentSpawnCount = 0;
+
+                    yield return WaitForSecondsRealtimeCustom(unit.TimeBetweenEachUnits, () => _isPaused);
+                }
+
+                _currentWaveIndex = w + 1;
+                _currentUnitIndex = 0;
+
+                yield return WaitForSecondsRealtimeCustom(wave.SpawnDelayBetweenWawe, () => _isPaused);
+            }
+
+            _spawnCoroutine = null;
+        }
+
+       
+
+        #region Helper Methods
+        
+        private IEnumerator WaitForSecondsRealtimeCustom(float duration, System.Func<bool> pauseCondition)
+        {
+            float elapsed = 0f;
+            while (elapsed < duration)
+            {
+                if (!pauseCondition())
+                {
+                    elapsed += Time.deltaTime;
+                }
+                yield return null;
+            }
+        }
+
+        private void OnGameStateChanged(GameState gameState)
+        {
+            if (gameState == GameState.Playing)
+            {
+                _isPaused = false;
+                _spawnCoroutine ??= StartCoroutine(SpawnFighter());
+            }
+            else
+            {
+                _isPaused = true;
             }
         }
         
@@ -57,23 +116,6 @@ namespace Managers
             _fighterPool.ReturnFighterToPool(baseFighter);
         }
 
-        #region Helper Methods
-
-        private void OnGameStateChanged(GameState gameState)
-        {
-            if (gameState == GameState.Playing)
-            {
-                _isSpawning = true;
-                _spawnCoroutine = StartCoroutine(SpawnFighter());
-            }
-            else
-            {
-                _isSpawning = false;
-                StopCoroutine(_spawnCoroutine);
-            }
-        }
-
         #endregion
-        
     }
 }
---
+++ Assets/Scripts/ScriptableObjects/LevelDataSO.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using Misc;
+using UnityEngine;
+
+namespace ScriptableObjects
+{
+    [CreateAssetMenu(fileName = "NewLevel", menuName = "ScriptableObject/Level", order = 0)]
+    public class LevelDataSO : ScriptableObject
+    {
+       public int LevelId;
+       public float TimeBetweenEachFighter;
+       public List<Wawe> Waves = new();
+    }
+
+    [System.Serializable]
+    public class Wawe
+    {
+       public List<Unit> Units = new();
+       public float SpawnDelayBetweenWawe;
+    }
+
+    [System.Serializable]
+    public class Unit
+    {
+        public FighterType FighterType;
+        public int UnitSpawnCount;
+        public int TimeBetweenEachUnits;
+    }
+}
